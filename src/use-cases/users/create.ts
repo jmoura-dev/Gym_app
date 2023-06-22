017d184109b0ce0f42ddd99c79a38927731cb920
Jackson <jackson1712@hotmail.com>
2023-06-22 12:56:11 -0300

    create register use-case and test to register use-case

---
+++ src/use-cases/users/create.ts
@@ -0,0 +1,42 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { hash } from 'bcryptjs'
+import { EmailAlreadyRegistered } from '../errors/verify-already-email'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+interface RegisterUseCaseResponse {
+  user: User
+}
+
+export class RegisterUserUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new EmailAlreadyRegistered()
+    }
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+
+    return {
+      user,
+    }
+  }
+}
