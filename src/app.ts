4e020378ae129be33c5818eb3e48d6a841eea34b
Jackson <jackson1712@hotmail.com>
2023-09-04 19:20:16 -0300

    refactor: change items to receive new features and your configs

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import { fastify } from 'fastify'
-import { usersRoutes } from './http/controllers/usersRoutes'
+import { usersRoutes } from './http/controllers/users/usersRoutes'
 import { ZodError } from 'zod'
 import { env } from './env'
+import fastifyJwt from '@fastify/jwt'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 
 app.register(usersRoutes)
 
--- src/http/controllers/usersRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { UsersController } from './UsersController'
-import { FastifyInstance } from 'fastify'
-
-const usersController = new UsersController()
-
-export async function usersRoutes(app: FastifyInstance) {
-  app.post('/users', usersController.register)
-}
